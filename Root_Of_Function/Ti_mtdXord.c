80d57c7ac7fcccc7fa1cfe6a156a72b532be8e47
tivole <asgarovkamran@gmail.com>
2019-11-12 23:49:35 +0400

    Rewrite Ti_mtdXord

--- Root_Of_Function/Ti_mtdXord.c
+++ Root_Of_Function/Ti_mtdXord.c
@@ -2,79 +2,90 @@
 
 #include <stdio.h>
 #include <math.h>
+
 #define eps 1.e-15
 
-double znak(double x) {
-	if(x>0) return 1;
-	else if(x<0) return -1;
+double sgn(double);
+double f(double);
+double find_root(double, double, double (*)(double), double*);
+
+
+int main(void){
+
+	double a, b, root_x;
+
+	scanf("%lf%lf", &a, &b);
+	
+	if (!find_root(a, b, f, &root_x)) {
+		printf("(!) Method is not allowed.\n");
+	} else {
+		printf("Root x of function f is: %lg\n", root_x);
+	}
+
+	return 0;
+}
+
+
+double sgn(double x) {
+	if (x > 0) return 1;
+	else if (x < 0) return -1;
 	else return 0;
 }
+
 
 double f(double x) {
 	return sin(x);
 }
 
-double koren(double a, double b, double (*f)(double)) {
-	double fa,fb,zfa,zfb;
-	fa=f(a); fb=f(b);
-	zfa = znak(fa);
-	zfb = znak(fb);
 
-	if(zfa == zfb){
-		printf("Metod ne primenim");
-		return 0;
-	}
+double find_root(double a, double b, double (*f)(double), double *root_x) {
+	double fa, fb, sgn_fa, sgn_fb, c, fc, sgn_fc;
 	
-	double c, fc, zfc;
-	while((fabs(b - a) > eps) && (fabs(b-a) > eps*fabs(a))) {
-		if(fabs(fa) < eps){
-			printf("%lf", a);
-			return 0;
+	fa = f(a);
+	fb = f(b);
+	
+	sgn_fa = sgn(fa);
+	sgn_fb = sgn(fb);
+
+	if (sgn_fa == sgn_fb) return 0;
+	
+	while ((fabs(b - a) > eps) && (fabs(b - a) > eps*fabs(a))) {
+		if (fabs(fa) < eps) {
+			*root_x = a;
+			return 1;
+		} else if (fabs(fb) < eps) {
+			*root_x = b;
+			return 1;
 		}
-		if(fabs(fb) < eps){
-			printf("%lf", a);
-			return 0;
-		}
-		c = b-((fb*(b-a))/(fb-fa));
+		
+		c = b - (fb*(b - a)) / (fb - fa);
 		fc = f(c);
-		zfc = znak(fc);
-		printf("%le\n", c);
-		if(fa>fb) {
-			if(zfc > 0){
+		sgn_fc = sgn(fc);
+
+		if (fa > fb) {
+			if (sgn_fc > 0){
 				a = c;
 				fa = fc;
-			}
-			else if(zfc < 0) {
+			} else if (sgn_fc < 0) {
 				b = c;
 				fb = fc;
+			} else if (sgn_fc == 0) {
+				*root_x = c;
+				return 1;
 			}
-			else if(zfc == 0) {
-				printf("%le", c);
-				return 0;
-			}
-		}
-		else if(fa < fb) {
-			if(zfc > 0) {
+		} else if (fa < fb) {
+			if (sgn_fc > 0) {
 				b = c;
 				fb = fc;
-			}
-			else if(zfc < 0) {
+			} else if (sgn_fc < 0) {
 				a = c;
 				fa = fc;
-			} else if(zfc == 0) {
-				printf("%le", c);
-				return 0;
+			} else if (sgn_fc == 0) {
+				*root_x = c;
+				return 1;
 			}
 		}
 	}
-	return 0;
-}
-
-int main(void){
-
-	double a,b;
-	scanf("%le%le", &a,&b);
-	koren(a,b,f);
 
 	return 0;
 }
