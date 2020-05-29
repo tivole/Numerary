722f8490014cddc95ac5fec0514889cf566620bd
tivole <asgarovkamran@gmail.com>
2020-05-29 13:05:56 +0400

    Add tests for diff method 1 and 2

--- Differentiation/First Order/Method 2/differentiation.c
+++ Differentiation/First Order/Method 2/differentiation.c
@@ -4,28 +4,21 @@
 #include <math.h>
 
 
-double differentiate(double (*)(double), double);
+double differentiate(double (*)(double), double, double);
 double f(double);
 double df(double);
+void test();
 
 
 int main(void) {
 
-    double x, dfx, real_dfx, error;
-
-    x = 3.1415926535897932384626;
-    dfx = differentiate(f, x);
-    real_dfx = df(x);
-    error = fabs(dfx - real_dfx);
-
-    printf("My method = %lg\nReal value = %lg\nError = %lg\n", dfx, real_dfx, error);
+    test();
 
     return 0;
 }
 
 
-double differentiate(double (*f)(double), double x) {
-    double h = 0.00001;
+double differentiate(double (*f)(double), double x, double h) {
     return (f(x + h) - f(x - h)) / (2*h);
 }
 
@@ -37,4 +30,25 @@ double f(double x) {
 
 double df(double x) {
     return cos(x);
+}
+
+void test() {
+    double dfx, real_dfx, error;
+
+    double h[] = {1e-3, 1e-4, 1e-5, 1e-7, 1e-9, 1e-11};
+    double x[] = {0.0, 1.0, 5.0};
+
+    for (int i = 0; i < 3; i++) {
+        printf("x = %lf\n", x[i]);
+        printf("-----------------------------------------------------------\n");
+        for (int j = 0; j < 6; j++) {
+            dfx = differentiate(f, x[i], h[j]);
+            real_dfx = df(x[i]);
+            error = fabs(dfx - real_dfx);
+
+            printf("h = %E\n", h[j]);
+            printf("My method = %lg\nReal value = %lg\nError = %lg\n\n", dfx, real_dfx, error);
+        }
+        printf("\n-----------------------------------------------------------\n\n");
+    }    
 }
